111cc15155df06e52492709327213708c450849f
rsandu1 <107316441+rsandu1@users.noreply.github.com>
2023-06-13 18:57:22 -0400

    Update home.js

--- static/home.js
+++ static/home.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['from Queens NY','go to Fordham University'],
+    strings:['am from Queens NY','go to Fordham University'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
